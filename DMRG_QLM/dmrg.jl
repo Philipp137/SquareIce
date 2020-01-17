b155e09312120bc720e62183bc6db55a5827baf1
paolostornati <paolo.stornati@gmail.com>
2020-01-17 14:59:53 +0100

    minor

--- DMRG_QLM/dmrg.jl
+++ DMRG_QLM/dmrg.jl
@@ -8,20 +8,20 @@ include("func_dmrg.jl")
 include("chess_operator.jl")
 s=16
 D= 20
-N = 21
+N = 9
 
 A = randmps(N, s, D);
-
+coupling_interaction=-10.0
 
 #M =  mpoqlm(N ; coupling=1.  );
 #M =  mpoqlm_fixed_boundary_contitions(N ; coupling=1.  );
-M =  mpoqlm_fixed_with_interaction(N ; coupling=0.0  );
+M =  mpoqlm_fixed_with_interaction(N ; coupling=coupling_interaction  );
 
 
 E, A, F = dmrgconvergence!(A, M ; verbose = true);
 
 N=N-1
-println("$N  $D    $E   ")
+println("$N  $D    $E  $coupling_interaction ")
 N=N+1
 
 sp = [0. 1.; 0. 0.]
@@ -35,15 +35,21 @@ O=kron(kron(kron(u,u),sz),sz)
 
 winding_number=measure1siteoperator(A,O)
 winding_number= deleteat!(winding_number , N)
-println(winding_number)
+#println(winding_number)
 
 
 
 println(sum(winding_number) )
 
 
-chess_operator=chess_operator_down(N)
+chess_down=chess_operator_down(N)
 
-down = measure_mpo!(A,chess_operator)
+chess_up=chess_operator_up(N)
 
-println(down)
+down = measure_mpo!(A,chess_down)
+
+up = measure_mpo!(A,chess_up)
+
+
+#println("Number_of_Plaquettes $N coupling_interaction $coupling_interaction Bond_dimention $D  Energy_GS  $E winding_number $(sum(winding_number)) chess_down $down chess_up $up ")
+println("$N                   $coupling_interaction                 $D                 $E     $(sum(winding_number))      $down     $up ")
