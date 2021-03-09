e30d1613b52aa0d636586c6f2837c755e7be6223
Philipp Krah <philipp.l.krah@campus.tu-berlin.de>
2021-03-09 11:46:27 +0100

    removes whitespaces from file names

--- 02_DMRG/src/io.jl
+++ 02_DMRG/src/io.jl
@@ -4,7 +4,8 @@ using FileIO
 function save_configuration( A, Lx, BondD, lambda; theta = 0.0 , mu = 0.0, Ly = 2, prefix = "SQ")
 
     fname = string(prefix , "_Lx-", Lx, "_Ly-", Ly, "_BondD-",BondD, "_lambda-",lambda,
-            "_theta-",theta, "_mu-", mu, ".jld2" )
+            "_theta-",theta, "_mux-", mu[1], "_muy-", mu[2], ".jld2" )
+    print("Loading: ", fname, "\n")
     print("Configuration saved in: ", fname ,"\n")
     save(fname, "MPS", A , "Lx", Lx, "D" , BondD , "lambda" , lambda , "theta" , theta , "mu", mu, "Ly", Ly)
 
@@ -13,7 +14,7 @@ end
 function load_configuration( Lx, BondD, lambda; theta = 0 , mu = 0, Ly = 2, prefix = "SQ")
 
     fname = string(prefix , "_Lx-", Lx, "_Ly-", Ly, "_BondD-",BondD, "_lambda-",lambda,
-            "_theta-",theta, "_mu-", mu, ".jld2" )
+            "_theta-",theta, "_mux-", mu[1], "_muy-", mu[2], ".jld2" )
     print("Loading: ", fname, "\n")
     dict = load(fname)
 
