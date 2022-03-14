4b04faec69cde740856d6e6221c76ff2952737b9
pkrah <philipp.l.krah@gmail.com>
2022-03-14 13:26:08 +0100

    small bugg

--- 02_DMRG/measure.jl
+++ 02_DMRG/measure.jl
@@ -8,12 +8,14 @@ include("src/io.jl")
 
 using Main.mpos: mpoqlm
 
-function measure(;config_name="./data/my_config.jld2",Lx= 0 , BondD=0 , Ly=0,theta = 0, coupling=0, mu=0, prefix = "SQ")
+function measure(;config_name="",Lx= 0 , BondD=0 , Ly=0,theta = 0.0, coupling=0.0, mu=0.0, prefix = "SQ")
 
      if config_name != ""
          dict  =load_configuration( config_name)
      else
-         dict = load_configuration( Lx, BondD, coupling; theta , mu, Ly, prefix)
+         mu_vec = [0.0, parse(Float64, mu)]
+         coupling = parse(Float64, coupling)
+         dict = load_configuration( Lx, BondD, coupling; theta , mu=mu_vec, Ly=Ly, prefix=prefix)
      end
 
      A =dict["MPS"]
@@ -21,19 +23,18 @@ function measure(;config_name="./data/my_config.jld2",Lx= 0 , BondD=0 , Ly=0,the
      D =dict["D"]
      lambda =dict["lambda"]
      theta  =dict["theta"]
-     mu =dict["mu"]
+     mu_vec =dict["mu"]
      Ly =dict["Ly"]
-     chemical_potential = [0, mu]
      if Ly == 2
          s = 16
      else
          println("Warning!!! Ly >2 not implemented")
      end
-     mpo = mpoqlm(Lx; Ly=Ly, coupling=lambda , mu=chemical_potential , theta= theta);
-     measure_op!(lambda , chemical_potential,  theta  , s, D, 10*D, A, mpo)
+     mpo = mpoqlm(Lx; Ly=Ly, coupling=lambda , mu=mu_vec , theta= theta);
+     measure_op!(lambda , mu_vec,  theta  , s, D, 10*D, A, mpo)
 end
 
 
 
 #measure(config_name=ARGS[1])
-measure(Lx= ARGS[1] , BondD=ARGS[2] , Ly=ARGS[3], coupling=ARGS[4], mu=ARGS[5], prefix = ARGS[7])
+measure(Lx= ARGS[1] , BondD=ARGS[2] , Ly=ARGS[3], coupling=ARGS[4], mu=ARGS[5], prefix = ARGS[6])
